2c94593c53a35d17ab72b536bc161286745e5e21
forge1010 <ale.silva1120@gmail.com>
2021-03-19 00:54:17 -0300

    validação do email iniciada

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function valid(){
+    var email =loginForm.email.value;
+    var regex=/[!-@]/
+
+var regex=/[@]/
+if(regex.test(email)==true){
+  alert("valido")
+}else{
+  var element= document.getElementById('email');
+    element.classList.add("red");
+    document.getElementById('error').innerHTML="Por favor insira um email valido"
+}
+}
